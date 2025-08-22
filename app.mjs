e8d1459fb9d8b4ca8fa15ae842c4a4054fd4f003
Frederique Andolfatto <pk37fki@eduvaud.ch>
2025-08-22 14:39:45 +0200

    contacts get without router

--- app.mjs
+++ app.mjs
@@ -1,6 +1,6 @@
 //On importe le module express (il doit être installé avec "npm install express")
 import express from 'express';
-import contactsRouter from './routes/contacts.mjs';
+import contacts from "./db/mock-contacts.mjs";
 // On crée une application Express
 const app = express();
 // On définit le port sur lequel le serveur va écouter
@@ -10,12 +10,17 @@ app.use(express.json());
 
 // On crée une route GET sur la racine ("/")
 // Quand un utilisateur ouvre http://localhost:3000/ dans son navigateur,
-// cette fonction est appelée et envoie "Hello World!" en réponse
-//app.get('/', (req, res) => {
-//    res.send('Hello World!')
-//})
+// cette fonction est appelée et envoie la liste des contacts en réponse
+app.get("/", (req, res) => {
+    res.json({contacts});
+});
 
-app.use('/api/contacts', contactsRouter);
+app.get("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const contact = contacts.find(contact => contact.id === id);
+    res.json({contact});
+});
+
 
 // On démarre le serveur et on lui dit d’écouter sur le port défini
 // Une fois lancé, le message est affiché dans la console
