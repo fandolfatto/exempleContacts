4dd260a5d6e2fb7f682df536c70098bb795ad66f
Frederique Andolfatto <pk37fki@eduvaud.ch>
2026-09-08 14:22:44 +0200

    change status and swagger mgt

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,8 +2,9 @@ import express from "express";
 import { db }  from "../db/db-contacts.js";
 import {isValidId, isValidEmail} from "../helper.js";
 
-//Code made in ase of success, we do not treat here the error case (id not existing for example)
-
+// As a general rule, for security reasons, we do not show the exact error message.
+// We prefer to display a generic message such as "An internal error occurred."
+// For pedagogical reasons, we sometimes show the actual error message, // in order to demonstrate how it works and what kind of messages can be returned.
 const contactsRouter = express.Router();
 
 /**
@@ -14,7 +15,7 @@ const contactsRouter = express.Router();
  *     description: get all contacts
  *     responses:
  *       200:
- *         description: Returns an array of contacts.
+ *         description: Returns an array of contacts
  *         content:
  *             application/json:
  *              schema:
@@ -29,13 +30,16 @@ contactsRouter.get("/", async(req, res) => {
         const contacts = await db.getAllContacts();
         res.json({contacts});
     } catch (error) {
-        res.status(500).json({error: error});
+        console.error(error);
+        res.status(500).json({
+            error: "Une erreur interne est survenue."
+        });
     }
 });
 
 contactsRouter.get("/:id", async(req, res) => {
     try {
-        const id = parseInt(req.params.id);
+        const id = Number(req.params.id);
         if (!isValidId(id)) {
             return res.status(400).json({ error: "ID invalide" });
         }
@@ -46,7 +50,10 @@ contactsRouter.get("/:id", async(req, res) => {
             res.json({contact});
         }
     } catch (error) {
-        res.status(500).json({error: error});
+        console.error(error);
+        res.status(500).json({
+            error: "Une erreur interne est survenue."
+        });
     }
 });
 
@@ -59,15 +66,22 @@ contactsRouter.post("/", async (req, res) => {
         }
         const newContact = await db.createContact({name, email});
         const message = `Le contact ${newContact.name} a bien été créé !`;
-        res.json({message: message, contact: newContact});
+        res.status(201).json({
+            message: message,
+            contact: newContact
+        });
     } catch (error) {
-        res.status(500).json({error: error});
+        console.error(error);
+        res.status(500).json({
+            error: "Une erreur interne est survenue."
+        });
     }
 });
 
 contactsRouter.put('/:id', async(req, res) => {
     try {
-        const id = parseInt(req.params.id);
+        const id = Number(req.params.id);
+        // we could check the id again, like for the get with id
         const {name, email} = req.body;
         const resUpdateContactNb = await db.updateContact(id, {name, email});
         if (resUpdateContactNb === 0) {
@@ -77,13 +91,17 @@ contactsRouter.put('/:id', async(req, res) => {
             res.json({message: 'Contact updated', contact : {contactUpdated}});
         }
     } catch (error) {
-        res.status(500).json({error: error});
+        console.error(error);
+        res.status(500).json({
+            error: "Une erreur interne est survenue."
+        });
     }
 });
 
 contactsRouter.delete('/:id', async(req, res) => {
     try {
-        const id = parseInt(req.params.id);
+        const id = Number(req.params.id);
+        // we could check the id again, like for the get with id
         let deletedContact = await db.deleteContact(id)
         if (deletedContact.success) {
             res.json({message: 'Contact deleted'});
@@ -91,7 +109,10 @@ contactsRouter.delete('/:id', async(req, res) => {
             res.status(404).json({error: "Contact non trouvé."});
         }
     } catch (error) {
-        res.status(500).json({error: error});
+        console.error(error);
+        res.status(500).json({
+            error: "Une erreur interne est survenue."
+        });
     }
 });
 
